c7bd8f059449e4b2b04fca0e12c9772b3aa883b2
artlu99 <alu99.sm@gmail.com>
2024-07-29 20:53:17 -0400

    /uses

--- src/components/Uses.tsx
+++ src/components/Uses.tsx
@@ -6,32 +6,94 @@ export const Uses = () => {
         <p>This is a list of the technologies used to build this site.</p>
         <ul>
           <li>
-            Vite + TypeScript + Preact + TailwindCSS + DaisyUI + Frontend Starter by{' '}
             <a
-              href="https://warpcast.com/farcasteradmin.eth"
-              alt=""
+              href="https://github.com/Borodutch/frontend-starter"
+              alt="frontend-starter"
               target="_blank"
               rel="noreferrer"
             >
-              @farcasteradmin.eth
+              Frontend Starter
+            </a>{' '}
+            by{' '}
+            <a
+              href="https://warpcast.com/farcasteradmin.eth"
+              alt="farcasteradmin.eth"
+              target="_blank"
+              rel="noreferrer"
+            >
+              borodutch
             </a>
+            : Vite + TypeScript + Preact + TailwindCSS + DaisyUI
           </li>
-          <li>Farcaster AuthKit</li>
-          <li>Wouter</li>
-          <li>Zustand</li>
-          <li>RemixIcons</li>
           <li>
-            react-farcaster-embed by{' '}
+            <a
+              href="https://github.com/pugson/react-farcaster-embed"
+              alt="react-farcaster-embed"
+              target={'_blank'}
+              rel="noreferrer"
+            >
+              react-farcaster-embed
+            </a>{' '}
+            by{' '}
             <a
               href="https://warpcast.com/pugson"
-              alt=""
+              alt="pugson"
               target="_blank"
               rel="noreferrer"
             >
               @pugson
             </a>
           </li>
-          <li>Cloudflare Pages</li>
+          <li>
+            <a
+              href="https://docs.pmnd.rs/zustand/getting-started/introduction"
+              alt="Zustand"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Zustand
+            </a>
+            ,{' '}
+            <a
+              href="https://github.com/molefrog/wouter"
+              alt="Wouter"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Wouter
+            </a>
+            , and{' '}
+            <a
+              href="https://remixicon.com"
+              alt="RemixIcons"
+              target="_blank"
+              rel="noreferrer"
+            >
+              RemixIcons
+            </a>
+          </li>
+          <li>
+            backend:{' '}
+            <a
+              href="https://pages.cloudflare.com"
+              alt="Cloudflare Pages"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Cloudflare Pages
+            </a>
+          </li>
+          <li>
+            Farcaster{' '}
+            <a
+              href="http://docs.farcaster.xyz/auth-kit/"
+              alt="AuthKit"
+              target="_blank"
+              rel="noreferrer"
+            >
+              AuthKit
+            </a>
+          </li>
         </ul>
       </div>
     </>
